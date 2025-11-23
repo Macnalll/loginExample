136d9ba32b5fb0ad97b7f72fbbc9f56f69e7627f
Macnalll <macnalll@tcd.ie>
2025-11-23 18:33:43 -0500

    CORS change

--- backend/index.js
+++ backend/index.js
@@ -12,21 +12,11 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 console.log("CORS index allowed origin:", FRONTEND_URL);
 
 // TODO: complete me (CORS)
-const corsOptions = {
-  origin: FRONTEND_URL,
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-    "Access-Control-Allow-Origin",
-  ],
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+);
 
 app.use(express.json());
 app.use('', routes);
